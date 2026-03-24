848e5e11cb9f0c7679c401842f6f9f1d355a5050
andresgaviria27 <andres_felipe_gaviria28@hotmail.com>
2026-03-23 19:27:55 -0500

    feat: init app/page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className='min-h-screen flex flex-col items-center justify-center bg-gray-950 text-white'>
+      <h1 className='text-5xl font-bold mb-4'>Diseño Pro</h1>
+      <p className='text-gray-400 text-xl'>Landing page para agencia de diseño que muestra servicios y portfolio de la agencia. Permite a los clientes contactar con la agencia de manera fácil y rápida.</p>
+      <div className='mt-8 px-6 py-3 bg-white text-black rounded-lg font-semibold'>Coming Soon</div>
+    </main>
+  )
+}
